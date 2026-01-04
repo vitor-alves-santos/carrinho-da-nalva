183b22f737ba70c97f838e63092cf3eed9d8b3ca
Diego Fagundes <fagundesdiego2015bolcombr@gmail.com>
2026-01-04 19:14:29 -0300

    fix: adds suppressHydrationWarning to fix hydration errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,9 +23,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning
       >
         {children}
       </body>
